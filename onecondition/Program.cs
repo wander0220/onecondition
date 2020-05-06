3a3a9f8bbd13f131f210e61c20a79453810334e8
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 15:29:21 +0900

    4번 문제 풀기 깜빡쓰하고 프로젝트 만들자마자 소스제어 안함;;;

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -14,14 +14,18 @@ namespace onecondition
             int x = int.Parse(Console.ReadLine());
 
 
-            if (x > 10)
-            {
-                if (x < 20)
-                {
-                    Console.WriteLine("조건에 맞습니다.");
-                }
-            }
+            //if (x > 10)
+            //{
+            //    if (x < 20)
+            //    {
+            //        Console.WriteLine("조건에 맞습니다.");
+            //    }
+            //}
 
+            if(x > 10 && x < 20)
+            {
+                Console.WriteLine("조건에 맞습니다.");
+            }
         }
     }
 }
