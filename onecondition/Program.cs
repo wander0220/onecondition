5c60a64e7d6a1fc6e586dadc9c5690dd831d042c
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 15:27:35 +0900

    초기 커밋.

---
+++ onecondition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onecondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("입력 : ");
+            int x = int.Parse(Console.ReadLine());
+
+
+            if (x > 10)
+            {
+                if (x < 20)
+                {
+                    Console.WriteLine("조건에 맞습니다.");
+                }
+            }
+
+        }
+    }
+}
